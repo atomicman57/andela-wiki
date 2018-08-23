8099ba3b805099c4b4003b21a3627db7f0170816
fokosun <okosunuzflorence@gmail.com>
2018-08-23 13:06:35 +0100

    footer links

---
+++ components/FooterLinks/Motivation.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+  <div>
+    <div>
+      <h3>I dont work at Andela</h3>
+      <p></p>
+    </div>
+
+    <div>
+      <h3>I work at Andela</h3>
+      <p>
+        Within the fellowship and in Andela generally, we generate tons of information
+        by the minute and buzz words. Keeping tabs on them becomes inevitably hard
+        to keep up with especially as we scale. Likewise, we also have tons of ways
+        to access information (google docs, drive, slack channels,
+        andela products even social media) but no single source of truth.
+        What if there is a goto place where one can go check the meaning of a
+        new buzz word from the learning department? What if someone wrote a wiki
+        about the first country director of Lagos and I have never met him but I am
+        able to learn about him from what fellows and employees who knew him wrote
+        about him while he was with Andela! What if there is just this single place
+        I can go to find so much information without having to browse slack channels,
+        what if i can create a wiki that has not been created about a subject I am
+        knowledgeable of and every other employee in the fellowship is a contributor,
+        you know just the way wikipedia actually works?
+        What if, what if ...
+      </p>
+    </div>
+  </div>
+);
