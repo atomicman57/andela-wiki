e148a3be3707d3d3e36d8158b2b94181a6e09a86
fokosun <okosunuzflorence@gmail.com>
2018-08-23 11:55:00 +0100

    WIP: still figuring out the footer links and what make sense

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,10 @@ import React, { Fragment } from 'react';
 export default () => (
   <div className="pagefooter">
     <div className="footerlinks">
-      <a href="#">Privacy policy</a>
-      <a href="#">About</a>
-      <a href="#">Terms and conditions</a>
-      <a href="#">Cookie statement</a>
-      <a href="#">Developers</a>
+      <a href="#">Motivation</a>
+      <a href="#">How to use</a>
+      <a href="#">Report a Bug</a>
+      <a href="#">Contribute</a>
     </div>
   </div>
 );
