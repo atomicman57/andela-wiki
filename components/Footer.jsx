04f5b18009da9fb6a400eabf47cec17638b35f42
fokosun <okosunuzflorence@gmail.com>
2018-08-23 13:05:44 +0100

    import footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,18 @@ import React, { Fragment } from 'react';
 export default () => (
   <div className="pagefooter">
     <div className="footerlinks">
-      <a href="#">Motivation</a>
-      <a href="#">How to use</a>
-      <a href="#">Report a Bug</a>
-      <a href="#">Contribute</a>
+      <a href="/motivation">
+        Motivation
+      </a>
+      <a href="/how-to-use">
+        How to use
+      </a>
+      <a href="/report-bug">
+        Report a Bug
+      </a>
+      <a href="https://github.com/atomicman57/andela-wiki">
+        Contribute
+      </a>
     </div>
   </div>
 );
