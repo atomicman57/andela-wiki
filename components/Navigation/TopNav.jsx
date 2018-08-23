3511d43ca9fa2793d94ecffb17f54d3dd9c0b353
fokosun <okosunuzflorence@gmail.com>
2018-08-23 12:05:08 +0100

    remove uneeded links

--- components/Navigation/TopNav.jsx
+++ components/Navigation/TopNav.jsx
@@ -2,10 +2,17 @@ import React, { Fragment } from 'react';
 
 export default () => (
   <div className="headerLinks">
-    <span className="user">Not logged in</span>
-    <a href="#">Talk</a>
-    <a href="#">Contributions</a>
-    <a href="#">Create account</a>
-    <a href="#">Log in</a>
+    <span className="user">
+      Not logged in
+    </span>
+    <a href="#">
+      Contributions
+    </a>
+    <a href="#">
+      Create account
+    </a>
+    <a href="#">
+      Log in
+    </a>
   </div>
 );
